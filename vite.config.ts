fcdeeb459b1a005b1b939bfcba2275f10b85f4dc
DoaaHazem <doaahazem711@gmail.com>
2025-09-10 20:11:34 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,1 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/portfolio/',  
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; // https://vitejs.dev/config/ export default defineConfig({ plugins: [react()], optimizeDeps: { exclude: ['lucide-react'], }, });
